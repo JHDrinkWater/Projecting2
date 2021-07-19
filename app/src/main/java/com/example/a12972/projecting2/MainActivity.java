a5e64dfb143f45e585595825c0bad7bb70a420be
zbk <1297234146@qq.com>
2021-07-19 15:31:17 +0800

    集成sdk

--- app/src/main/java/com/example/a12972/projecting2/MainActivity.java
+++ app/src/main/java/com/example/a12972/projecting2/MainActivity.java
@@ -2,12 +2,42 @@ package com.example.a12972.projecting2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.ximalaya.ting.android.opensdk.datatrasfer.CommonRequest;
+import com.ximalaya.ting.android.opensdk.datatrasfer.IDataCallBack;
+import com.ximalaya.ting.android.opensdk.model.category.Category;
+import com.ximalaya.ting.android.opensdk.model.category.CategoryList;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Map<String, String> map = new HashMap<String, String>();
+        CommonRequest.getCategories(map, new IDataCallBack<CategoryList>() {
+            @Override
+            public void onSuccess(CategoryList object) {
+                List<Category> categories = object.getCategories();
+                int size = categories.size();
+                Log.d(TAG, "onSuccess: Category size ------> " + size);
+                for (Category category : categories) {
+                    Log.d(TAG, "onSuccess: category name -------> " + category.getCategoryName());
+                }
+            }
+
+            @Override
+            public void onError(int code, String message) {
+                Log.d(TAG, "onError: error code" + code + " error message " + message);
+            }
+        });
     }
 }
